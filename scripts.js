c1e3e983c8d2c8b747fde1b87c74d895cda16461
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-17 12:17:46 +0600

    post working

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ document.getElementById('getText').addEventListener('click', getText);
 document.getElementById('getUser').addEventListener('click', getUser);
 const user_data = document.getElementById('Users')
 document.getElementById('getPost').addEventListener('click', getPost);
+document.getElementById('addPost').addEventListener('submit', addPost);
 
 // let output
 
@@ -26,7 +27,6 @@ function getUser(){
                         <li>Name: ${user.Name}</li>
                         <li>Desc: ${user.Desc}</li>
                     </ul>
-            
             `;
             
         });
@@ -47,11 +47,29 @@ function getPost(){
                         <h3>${post.title}</h3>
                         <p>${post.body}</p>
                     </div>
-            
             `;
         });
 
         document.getElementById('Posts').innerHTML = output
         document.getElementById('Posts').removeEventListener("click", getUser);
     })
+}
+
+
+function addPost(e){
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'ACCEPT': 'application/json, text/plain, */*',
+            'Content-type': 'application/json' 
+        },
+        body:JSON.stringify({title:title, body:body})
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
 }
